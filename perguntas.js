a73e581b334de0dfd6ed32190365a93ac295a35f
thamii1 <00001091444523sp@al.educacao.sp.gov.br>
2024-11-25 02:17:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Rapunzel',
+    'O que a bruxa queria de Rapunzel?',
+    'A bruxa queria manter Rapunzel presa para usar seu cabelo mágico.'
+)
+
+criaCartao(
+    'Rapunzel',
+    'Como Rapunzel foi trancada na torre pela primeira vez?',
+    'A bruxa a sequestrou quando ainda era bebê, após os pais dela roubarem rabanetes da horta da bruxa.'
+)
+
+criaCartao(
+    'Rapunzel',
+    'Qual era o segredo do cabelo de Rapunzel?',
+    'O cabelo de Rapunzel tinha poderes mágicos que podiam curar e rejuvenescer a bruxa.'
+)
+
+criaCartao(
+    'Rapunzel',
+    'O que aconteceu quando a bruxa descobriu que Rapunzel havia se encontrado com o príncipe?',
+    'A bruxa cortou o cabelo de Rapunzel e a exilou, enganando o príncipe e cegando-o.'
+)
